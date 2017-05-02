52ed18b8b325442de96e6403851132e5be9efa74
Симонова Валерия Владимировна <mm51s15@awp08.csc.local>
2017-05-02 12:53:24 +0700

    New code

--- command.c
+++ command.c
@@ -1,30 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include<math.h>
+#include <inttypes.h>
 #include"command.h"
 
+enum {
+    MaxCodeLength = 4
+};
+
+ struct str {
+    uint8_t code[MaxCodeLength];
+    size_t length;
+} ;
 
 
 int encode_file( const char *entry, const char *exit){
-    char mass[20];
-	int dv=0,c=1;
+   uint32_t m; 
+   int l=0;
+   int code[100];
+   unsigned long b;
+   int c=1;
     FILE* vvod;
     vvod=fopen(entry,"r");
-    fgets(mass,20, vvod);
-    unsigned long x=strtoul(mass,0,0);
-    printf("%lu\n",x);
-    fclose(vvod);
-    FILE* vivod;
-    vivod=fopen(exit,"w"); 
-    while(x){
-	dv+=(x%2)*c;
-	c*=10;
-	x/=2;}
-printf("%d\n",dv);
+    fscanf(vvod, "%" SCNx32, &m);
+    printf("%" PRIx32 "\n", m);
+    for(int i=0;i<8;i++){
+    code[i]=i<<2;
+    }
+     for(int i=0;i<8;i++){
+    printf("%d",code[i]);
+    }
+     fclose(vvod);
+     
+     printf("%d\n",b);
+   
     
-
-
-
+     
+       
 }
 
 int decode( const char *entry, const char *exit){
--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 #include"command.h"
-int main(){
-
- const char vvod[100]="text.txt";
- const char vivod[100]="dva.txt";
- encode_file(vvod,vivod);
+int main( int argc,char argv[]){
+ for (i = 0; i < argc; ++i) {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+    if(argv[1]=="encode")
+    encode_file(argv[1],argv[2]);
+    if(argv[1]=="decode")
+    decode_file(argv[1],argv[2]);
 return 0;
 }
